9a5ec4bd354884dae5b60d03cdf1eedf32855362
Brady Tran <baotrank22tpm4@gmail.com>
2020-10-23 14:49:52 +0700

    ruud

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'ruud';
+	numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+	reload() {
+    location.reload();
+  }
 }
